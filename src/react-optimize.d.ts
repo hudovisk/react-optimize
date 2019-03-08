ddfa993fd99459589606dbec1e18dfa634341ac6
Alex Hughes <alxhghs@gmail.com>
2019-03-08 10:47:05 -0500

    add comment to types file

--- src/react-optimize.d.ts
+++ src/react-optimize.d.ts
@@ -1,14 +1,21 @@
+declare module "react-optimize" {
+
+// Type definitions for react-optimize v1.0.2
+// Project: https://github.com/hudovisk/react-optimize
+// Definitions by: Alex Hughes <https://github.com/alxhghs>
+// TypeScript Version: 3.3.3333
+
 import React from "react";
 
-declare module "react-optimize" {
-    export type ExperimentProps = {
-        children: React.ReactNode;
-        id: string;
-    }
-    export class Experiment extends React.Component<ExperimentProps> {}
-    export type VariantProps = {
-        children: React.ReactNode;
-        id: string;
-    }
-    export class Variant extends React.Component<VariantProps> {}
+export type ExperimentProps = {
+    children: React.ReactNode;
+    id: string;
+}
+export class Experiment extends React.Component<ExperimentProps> {}
+export type VariantProps = {
+    children: React.ReactNode;
+    id: string;
+}
+export class Variant extends React.Component<VariantProps> {}
+
 }
