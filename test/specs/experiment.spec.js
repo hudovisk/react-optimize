0a1e06ae275fb3878e4d909538a73d640b6d2220
Corentin Ardeois <cardeois@landr.com>
2022-03-07 12:00:47 -0500

    fix: state leak when component is unmounted
    When the component was unmounted before Google Optimize
    was loaded, we had a state update that was causing an exception
    on dev mode
    
    Check if the component is unmounted before updating the state
    fixes the issue

--- test/specs/experiment.spec.js
+++ test/specs/experiment.spec.js
@@ -4,6 +4,11 @@ import sinon from "sinon";
 import { Experiment } from "../../src";
 
 describe("experiment", () => {
+  afterEach(() => {
+    delete window.google_optimize;
+    delete window.dataLayer;
+  });
+
   it("should require experiment id", () => {
     expect(() => shallow(<Experiment />)).to.throw();
   });
@@ -49,6 +54,33 @@ describe("experiment", () => {
       const wrapper = shallow(<Experiment id="abc" loader={<Loader />} />);
 
       expect(wrapper.find(Loader)).to.have.lengthOf(1);
+    });
+
+    it("should update variant after optimize is loaded", () => {
+      delete window.dataLayer;
+      const wrapper = shallow(<Experiment id="abc" />);
+
+      expect(wrapper.state("variant")).to.be.equal(null);
+
+      // Load google optimize
+      window.google_optimize = { get: sinon.stub().returns("2") };
+      window.dataLayer.hide.end();
+
+      expect(wrapper.state("variant")).to.be.equal("2");
+    });
+
+    it("should not update variant after optimize is loaded if component was unmounted", () => {
+      delete window.dataLayer;
+      const wrapper = shallow(<Experiment id="abc" />);
+
+      expect(wrapper.state("variant")).to.be.equal(null);
+
+      wrapper.unmount();
+      // Load google optimize
+      window.google_optimize = { get: sinon.stub().returns("2") };
+
+      // If component state is updated while component is unmounted, this call will crash
+      window.dataLayer.hide.end();
     });
   });
 });
